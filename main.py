deb3f62d085bf9fe0d57263818821fb6dfea0da1
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2023-11-20 10:23:31 +0000

    added working code snippet for one stock and unstacked data

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-# test script
+"""
+Created on: 20 Nov 2023
+Created by: Philip P
 
+Script for scraping yfinance for US security prices & another source for mutual fund prices
+"""
 # third party imports
-
 import numpy as np
 import pandas as pd
 import yfinance
 
 if __name__ == '__main__':
-    pass
+    example_px = yfinance.download(
+        tickers='MSFT',
+        start='2023-11-16',
+        end='2023-11-17'
+    )
+
+    # unstack the pd dataframe
+    example_price_df = example_px.unstack().reset_index()
+    example_price_df.columns = ['measure', 'date', 'price']
+
+    # keep only the adjusted close price
+    price_df = example_price_df.loc[example_price_df['measure'] == 'Adj Close'].copy()
+    
