a733f3fba86f5cda15668ebd70403d2d34d3c3f2
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2023-12-01 18:23:35 +0000

    substitutions based on the sqlalchemy imports

--- portfolio-tracker/models.py
+++ portfolio-tracker/models.py
@@ -2,6 +2,7 @@
 
 # third party imports
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Model, String, Float, Boolean, DateTime, ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
@@ -9,10 +10,10 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), index=True, unique=True)
+    email = Column(String(120), index=True, unique=True)
+    password_hash = Column(String(128))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -23,34 +24,33 @@ class User(db.Model):
 class Stock(db.Model):
     __tablename__ = 'stocks'
 
-    id = db.Column(db.Integer, primary_key=True)
-    ticker = db.Column(db.String(10), unique=True, nullable=False)
-    name = db.Column(db.String(64), nullable=False)
+    id = Column(Integer, primary_key=True)
+    ticker = Column(String(10), unique=True, nullable=False)
+    name = Column(String(64), nullable=False)
 
 class Transactions(db.Model):
     __tablename__ = 'transactions'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) # add in foreign key
-    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.id')) # add in foreign key
-    transaction_type = db.Column(db.Boolean, nullable=False)
-    quantity_shares = db.Column(db.Integer, nullable=False)
-    date_transaction = db.Column(db.DateTime, nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id')) # add in foreign key
+    stock_id = Column(Integer, ForeignKey('stocks.id')) # add in foreign key
+    transaction_type = Column(Boolean, nullable=False)
+    quantity_shares = Column(Integer, nullable=False)
+    date_transaction = Column(DateTime, nullable=False)
 
 class Portfolio(db.Model):
     __tablename__ = 'portfolio'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(64), db.ForeignKey('users.id')) # add in foreign key
-    stock_id = db.Column(db.String(10), db.ForeignKey('stocks.id'))
-    total_shares = db.Column(db.Integer, nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(64), ForeignKey('users.id')) # add in foreign key
+    stock_id = Column(String(10), ForeignKey('stocks.id'))
+    total_shares = Column(Integer, nullable=False)
 
 
 class SecurityPrices(db.Model):
     __tablename__ = 'security_prices'
 
-    price_id = db.Column(db.Integer, primary_key=True)
-    stock_id = db.Column(db.String(10), db.ForeignKey('stocks.id'))
-    date = db.Column(db.DateTime, nullable=False)
-    close_price = db.Fkiat
-    
+    price_id = Column(Integer, primary_key=True)
+    stock_id = Column(String(10), ForeignKey('stocks.id'))
+    date = Column(DateTime, nullable=False)
+    close_price = Column(Float, nullable=False)
