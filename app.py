15732b5bb7b5d363b1dc7fec4cae0cc5c3a4fc24
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2023-12-07 18:17:28 +0000

    modified app.py to setup database on local drive

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-# created on Thursday 30 Nov
+"""
+Created on: 7 Dec 2023
+
+App file to initialise the Flask application and link it to a SQL Alchemy db
+"""
+
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from models import db, User, Stock, Transactions, Portfolio, SecurityPrices
 
+# Initiate a Flask app
 app = Flask(__name__)
-app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# Configure the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database with the app
+db.init_app(app)
+
+# Create tables
+with app.app_context():
+    db.create_all()
