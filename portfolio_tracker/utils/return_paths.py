c01322b94922ba5373060adae0c9ef459e4d8a42
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2023-12-14 10:46:37 +0000

    added return paths script

---
+++ portfolio_tracker/utils/return_paths.py
@@ -0,0 +1,18 @@
+"""
+Created on: 14 Dec 2023
+Utility functions to return useful paths
+"""
+
+import os
+
+
+def get_project_dir() -> str:
+    """
+    Retrieve the path of the project directory, going up two levels in the folder structure.
+
+    Returns:
+        str: The absolute path of the project directory.
+    """
+    current_dir = os.getcwd()
+    project_dir = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+    return project_dir
