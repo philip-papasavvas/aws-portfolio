50772fed1459c500ce1a987add74cb1eeb42b1d4
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2024-01-29 17:01:29 +0000

    removed non AWS code

--- portfolio_tracker/scripts/populate_db_stocks_from_json.py
+++ portfolio_tracker/scripts/populate_db_stocks_from_json.py
@@ -2,44 +2,18 @@
 Created on: 12 Dec 2023
 Script to populate the Stock database table with stock data from a JSON file
 """
-import os
 import json
+import os
 from typing import Dict
 
-from sqlalchemy.exc import IntegrityError
-from flask import current_app
 import psycopg2
 
 from portfolio_tracker import create_app
-from portfolio_tracker.models.models import Stock, db
 from portfolio_tracker.utils.return_paths import get_project_dir
 
 app = create_app()
 with open(os.path.join(get_project_dir(), 'portfolio_tracker', 'config', 'credentials.JSON')) as file:
     credentials = json.load(file)
-
-
-def populate_stocks_db_table():
-    """
-    Reads stock data from a JSON file and populate the Stock table in the database. The function looks for
-    a 'stocks.json' file in the 'config' directory relative to the current application's root path.
-    It expects the JSON file to contain a list of dictionaries, each with 'ticker' and 'name'
-    keys corresponding to the stock's ticker symbol and full name. Each stock from the JSON file is
-    added to the database session and committed to the Stock table. If an IntegrityError occurs during
-    commit, the transaction is rolled back.
-    """
-    json_path = os.path.join(current_app.root_path, 'config', 'stocks.json')
-    with open(json_path, 'r') as file:
-        stock_list = json.load(file)
-
-    for stock_info in stock_list:
-        stock = Stock(ticker=stock_info['ticker'], name=stock_info['name'])
-        db.session.add(stock)
-
-    try:
-        db.session.commit()  # commit the session to the database
-    except IntegrityError:
-        db.session.rollback()  # rollback the transaction if an error occurs
 
 
 def populate_stocks_db_table_aws(credentials: Dict[str, str]):
@@ -87,9 +61,4 @@ def populate_stocks_db_table_aws(credentials: Dict[str, str]):
 
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     # populate the database
-    #     populate_stocks_db_table()
-    #     print("The database table 'stocks' has been populated with the stock data")
-
     populate_stocks_db_table_aws()
